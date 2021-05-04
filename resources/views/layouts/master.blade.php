a5a9d94feb0a999edb486287bef39bef7a63c35e
wombat <wombat@wombat.wobat>
2021-05-04 12:30:50 -0400

    fix:css problem

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
     <title>Context Mobile</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="/css/app.css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @stack('scripts')
 
 </head>
