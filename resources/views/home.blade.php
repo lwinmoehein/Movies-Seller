34f64e17e2eb56ad790da52b0f74f6f92770d295
lwinmoehein <dev.lwinmoehein@gmail.com>
2021-06-28 07:12:10 +0630

    change:user app ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,13 +6,11 @@
       <a href="{{route('series.index')}}">
         <div class="series">
             <div class="title">Series <i class="fa fa-tv"></i></div>
-            {{-- <div class="count">Total : <span>{{$seriesCount}}</span> series</div> --}}
         </div>
       </a>
       <a href="{{route('movies.index')}}">
         <div class="movies">
             <div class="title">Movies <i class="fa fa-film"></i></div>
-            {{-- <div class="count">Total : <span>{{$moviesCount}}</span> movies</div> --}}
         </div>
       </a>
 
